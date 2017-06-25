c63cd33e5bd11612cdadb7843d89dd862ed394f3
Axel Nilsson <an222yp@student.lnu.se>
2017-06-25 07:48:29 +0200

    Added comments to runner and runner_test

--- lib/runner.go
+++ lib/runner.go
@@ -2,14 +2,23 @@ package lib
 
 import "os/exec"
 
-var runner Runner = RealRunner{}
+var runner Runner = RealRunner{} // Global variable used to execute shell commands
 
+/**
+ * The runner interface is used by RealRunner and is there so RealRunner can be mocked when testing.
+ */
 type Runner interface {
 	run(string, ...string) ([]byte, error)
 }
 
+/**
+ * RealRunner is the object used when executing commands, adhere to the Runner interface.
+ */
 type RealRunner struct{}
 
+/**
+ * The run method of RealRunner. Calls the exec.Command method with the command string and any arguments.
+ */
 func (RealRunner) run(cmd string, args ...string) ([]byte, error) {
 	return exec.Command(cmd, args...).CombinedOutput()
 }
--- lib/runner_test.go
+++ lib/runner_test.go
@@ -5,6 +5,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+/**
+ * Tests the RealRunner by running the echo command and validating the returned results
+ */
 func TestRealRunnerRun(t *testing.T) {
 	runner = RealRunner{}
 	results, err := runner.run("echo", "This is a test")
