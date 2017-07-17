565b58ed3568664d1e62ec6ba94ba485dd0811f2
axnion <an222yp@student.lnu.se>
2017-07-17 20:34:26 +0200

    Restructured project

--- app.go
+++ app.go
@@ -1,34 +1,5 @@
 package main
 
-import (
-	"flag"
-	"log"
-	"net/http"
-
-	"github.com/axnion/hrdwr/server"
-)
-
-var addr = flag.String("addr", ":8080", "http service address")
-var clientBuild = "client/build/"
-
 func main() {
-	fs := http.FileServer(http.Dir(clientBuild + "static"))
 
-	http.HandleFunc("/", serveClient)
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		server.ServeWebSocket(w, r)
-	})
-
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	err := http.ListenAndServe(*addr, nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
-
-}
-
-func serveClient(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	http.ServeFile(w, r, clientBuild+"index.html")
 }
