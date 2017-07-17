72ee359698d9cd3f389f9a07a239b2620c6f0432
axnion <an222yp@student.lnu.se>
2017-07-17 22:19:30 +0200

    Started on influxdb implementation

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM golang:latest
+
+WORKDIR /go/src/hrdwr
+COPY . .
+
+RUN go-wrapper download   # "go get -d -v ./..."
+RUN go-wrapper install    # "go install -v ./..."
+
+CMD ["go-wrapper", "run"] # ["hrdwr"]
--- hrdwr.go
+++ hrdwr.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/influxdata/influxdb/client/v2"
+)
+
+const addr = "http://influxdb:8086"
+const db = "hrdwr"
+
+// TODO: Add hostname to data being sent in to influx
+
+func main() {
+	influx, err := client.NewHTTPClient(client.HTTPConfig{
+		Addr: addr,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+
+	batch, err := client.NewBatchPoints(client.BatchPointsConfig{
+		Database:  db,
+		Precision: "s",
+	})
+
+	err = influx.Write(batch)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
